f9cb4c4cab8728a011b23c3ed2b588daf67c338e
rafoun <rumlerrafael@gmail.com>
2026-09-11 13:44:43 +0200

    we

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import { Server } from "socket.io";
 import { customAlphabet } from "nanoid";
 import { createInitialGameState } from "./game/state.js";
 import { applyAction } from "./game/actions.js";
+const ZombicideGame = require('./game/zombicideEngine');
 
 const nanoid = customAlphabet("ABCDEFGHJKLMNPQRSTUVWXYZ23456789", 5);
 
